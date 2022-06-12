00b87df60d1a6dddbf7374d3dfa9f13499e820b7
Gijs Groote <g.s.groote@student.tudelft.nl>
2022-06-13 00:58:09 +0200

    renamed export path variable name

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# neovim-for-beginner
+# NeoVIM configuration files by Gijs Groote
 
-Companion code for the [Medium Neovim for Beginners series.](https://alpha2phi.medium.com/learn-neovim-the-practical-way-8818fcf4830f#545a)
+### inspired by tutorial [Medium Neovim for Beginners series.](https://alpha2phi.medium.com/learn-neovim-the-practical-way-8818fcf4830f#545a)
--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/sh
 
-NVIM_BEGINNER=~/.config/
-export NVIM_BEGINNER
+NVIM_CONFIG_PATH=~/.config/
+export NVIM_CONFIG_PATH
 
-rm -rf $NVIM_BEGINNER
+rm -rf $NVIM_CONFIG_PATH
 
-mkdir -p $NVIM_BEGINNER/share
-mkdir -p $NVIM_BEGINNER/nvim
+mkdir -p $NVIM_CONFIG_PATH/share
+mkdir -p $NVIM_CONFIG_PATH/nvim
 
-stow --restow --target=$NVIM_BEGINNER/nvim .
+stow --restow --target=$NVIM_CONFIG_PATH/nvim .
 
-alias vim='XDG_DATA_HOME=$NVIM_BEGINNER/share XDG_CONFIG_HOME=$NVIM_BEGINNER nvim' 
+# I am calling this vim, while it actually is Neo VIM
+alias vim='XDG_DATA_HOME=$NVIM_CONFIG_PATH/share XDG_CONFIG_HOME=$NVIM_CONFIG_PATH nvim' 
 
 export vim 
